7528ba078921fd802fc5a0918f852c7593ca567a
Caroline Muscara <cjm72@bu.edu>
2025-04-26 13:18:57 -0400

    sign in page

---
+++ app/signin/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import styled from "styled-components";
+
+const SignInContainer = styled.div`
+`;
+
+const SignInButton = styled.a`
+`;
+
+const SignInText = styled.h1`
+`;
+
+export default function SignIn() {
+    const githubAuthUrl = ` https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GITHUB_REDIRECT_URI}&scope=user:email`;
+    return (
+        <SignInContainer>
+            <SignInText>Sign in to your account</SignInText>
+            <SignInButton href={githubAuthUrl}>Sign in with GitHub</SignInButton>
+        </SignInContainer>
+    );
+}
